5d4714a95d85d70fbc0e04f89fd2bbcb5e103c78
TinaCheong <kanade11020512@gmail.com>
2020-05-18 18:13:30 +0800

    <LollipopRemoteDataSource> setup an object to implement the LollipopDataSource function

---
+++ app/src/main/java/com/tina/lollipopcodingtest/data/source/remote/LollipopRemoteDataSource.kt
@@ -0,0 +1,25 @@
+package com.tina.lollipopcodingtest.data.source.remote
+
+import androidx.lifecycle.LiveData
+import com.tina.lollipopcodingtest.data.News
+import com.tina.lollipopcodingtest.data.NewsListResult
+import com.tina.lollipopcodingtest.data.Result
+import com.tina.lollipopcodingtest.data.source.LollipopDataSource
+import com.tina.lollipopcodingtest.network.LollipopApi
+
+object LollipopRemoteDataSource : LollipopDataSource {
+
+    override suspend fun getNewsByNetwork(nextPage: String): Result<NewsListResult> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    override fun getNewsByDatabase(): LiveData<List<News>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun insertNews(news: News) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
